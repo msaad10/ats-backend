3bfe08acc49e66bfef84b33429429c052d95f87e
anas <muhammad.anas@hq.bill.com>
2025-04-12 15:15:56 +0500

    update apply job api

--- src/main/java/com/ats/service/JobCandidateService.java
+++ src/main/java/com/ats/service/JobCandidateService.java
@@ -1,9 +1,13 @@
 package com.ats.service;
 
 import com.ats.dto.jobcandidate.JobCandidateRequest;
+import com.ats.model.Job;
 import com.ats.model.JobCandidate;
 import com.ats.model.CandidateStage;
+import com.ats.model.User;
 import com.ats.repository.JobCandidateRepository;
+import com.ats.repository.JobRepository;
+import com.ats.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -15,15 +19,32 @@ public class JobCandidateService {
 
     @Autowired
     private JobCandidateRepository jobCandidateRepository;
+    @Autowired
+    private JobRepository jobRepository;
+    @Autowired
+    private UserRepository userRepository;
 
     public JobCandidate createJobCandidate(JobCandidateRequest request) {
         if (jobCandidateRepository.existsByJobIdAndUserId(
-                request.getJobId(), 
+                request.getJobId(),
                 request.getUserId())) {
             throw new RuntimeException("User has already applied for this job");
         }
-        
+
+        Job job = jobRepository.findById(request.getJobId()).orElse(null);
+        User user = userRepository.findById(request.getUserId()).orElse(null);
+
+        if (job == null) {
+            throw new RuntimeException("Job not found");
+        }
+
+        if (user == null) {
+            throw new RuntimeException("User not found");
+        }
+
         JobCandidate jobCandidate = new JobCandidate();
+        jobCandidate.setJob(job);
+        jobCandidate.setUser(user);
         jobCandidate.setNotes(request.getNotes());
         jobCandidate.setCurrentStage(CandidateStage.APPLIED.toString());
         jobCandidate.setAppliedAt(LocalDateTime.now());
